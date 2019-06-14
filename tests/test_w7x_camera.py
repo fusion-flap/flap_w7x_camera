e7ce1ee44cb5687c01ede5a2b26ad3431b49780e
Gabor Cseh <cseh.gabor@wigner.mta.hu>
2019-06-14 10:37:26 +0200

    Fixing a typo.

--- tests/test_w7x_camera.py
+++ tests/test_w7x_camera.py
@@ -15,7 +15,7 @@ flap_w7x_camera.register()
 def test_get_data():
     try:
         flap.get_data('W7X_CAMERA',exp_id="20181018.032", name="AEQ20_EDICAM_ROIP1", options={'Time': '154804'}, object_name="EDI_ROIP1")
-    except Exception  as e:
+    except Exception as e:
         raise e
     flap.list_data_objects()
     flap.slice_data('EDI_ROIP1',slicing={'Time':0}, output_name='EDI_sliced')
